7889fd0017ad8682ee3fb14664c886eef6ac5826
eloaconrado-cyber <eloa.conrado@escola.pr.gov.br>
2026-06-23 12:02:23 -0300

    o

--- perguntas.js
+++ perguntas.js
@@ -5,14 +5,14 @@ criaCartao(
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'portugues',
+    'Qual é o plural correto da palavra "cidadão?',
+    'Cidadãos'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
+    ''',
+    'O que estuda a Citologia?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
@@ -22,3 +22,13 @@ criaCartao(
     'Oi em ingles é HI (RAI)'
 )
 
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
